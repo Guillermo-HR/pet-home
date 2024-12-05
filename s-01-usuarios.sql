419465580dd6ed6f8c3fbbf48864d547dbfa513e
Guillermo-HR <guillermo.hre@gmail.com>
2024-12-05 00:20:47 -0600

    Actualiza script de creación de usuarios y roles, corrige condiciones de búsqueda y agrega permisos para crear triggers y procedimientos.

--- s-01-usuarios.sql
+++ s-01-usuarios.sql
@@ -2,22 +2,20 @@
 --@Fecha creación: 14/11/2024
 --@Descripción: Creación de usuarios
 
--- Conectarse a la base de datos
 PROMPT ========================================================
-PROMPT Conectando a la PDB
-CONNECT sys/system1@ghrbd_s1 AS SYSDBA
+PROMPT Creación de usuarios
 PROMPT s-01-usuarios.sql
 PROMPT ========================================================
 
 -- Limpiar usuarios y roles
-PROMPT Limpiando usuarios y roles usados en la practica
+PROMPT Limpiando usuarios y roles usados en el proyecto
 DECLARE 
     CURSOR cur_usuarios IS 
         SELECT username FROM dba_users WHERE 
-            username LIKE 'ah%'; 
+            username LIKE 'AH%'; 
     CURSOR cur_roles IS 
         SELECT role FROM dba_roles WHERE 
-            role like 'ah'; 
+            role like 'AH%'; 
 BEGIN
     FOR r IN cur_usuarios LOOP 
         EXECUTE IMMEDIATE 'DROP USER '||r.username||' CASCADE'; 
@@ -31,7 +29,7 @@ END;
 -- Crear roles
 PROMPT creando roles
 CREATE ROLE ah_admin_rol;
-GRANT CREATE SESSION, CREATE TABLE, CREATE SEQUENCE, CREATE VIEW TO ah_admin_rol;
+GRANT CREATE SESSION, CREATE TABLE, CREATE SEQUENCE, CREATE VIEW, CREATE TRIGGER, CREATE PROCEDURE TO ah_admin_rol;
 
 CREATE ROLE ah_invitado_rol;
 GRANT CREATE SESSION TO ah_invitado_rol;
@@ -43,9 +41,3 @@ GRANT ah_admin_rol TO ah_proy_admin_rol;
 
 CREATE USER ah_proy_invitado IDENTIFIED BY contrasena QUOTA UNLIMITED ON USERS;
 GRANT ah_invitado_rol TO ah_proy_invitado;
-
--- Salir de la base
-PROMPT ========================================================
-PROMPT Saliendo de la PDB
-DISCONNECT;
-PROMPT ========================================================
